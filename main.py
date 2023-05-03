04daa5c39d51695fa2735594088c3e1aebce938e
vasa <vas.markovic@gmail.com>
2023-05-03 18:16:02 +0200

    Fix to load env in python instead of an ugly bash script

--- main.py
+++ main.py
@@ -1,76 +1,43 @@
-import agent
+
 import os
-from agent import Agento
 from dotenv import load_dotenv
 from api import start_api_server
-import importlib
-from replacement_chains import Agent
-import uvicorn
 API_ENABLED = os.environ.get("API_ENABLED", "False").lower() == "true"
+import boto3
 
-# Load default environment variables (.env)
-load_dotenv()
 
-# def can_import(module_name):
-#     try:
-#         importlib.import_module(module_name)
-#         return True
-#     except ImportError:
-#         return False
+def fetch_secret(secret_name, region_name, env_file_path):
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
 
-# AGENT_NAME = os.getenv("AGENT_NAME") or "my-agent"
-#
-# agent = Agent(AGENT_NAME)
-#
-# # Creates Pinecone Index
-# agent.createIndex()
-# API Keys
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-# assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+    except Exception as e:
+        print(f"Error retrieving secret: {e}")
+        return None
 
-# OPENAI_API_MODEL = os.getenv("OPENAI_API_MODEL", "gpt-3.5-turbo")
-# assert OPENAI_API_MODEL, "OPENAI_API_MODEL environment variable is missing from .env"
+    if "SecretString" in response:
+        secret = response["SecretString"]
+    else:
+        secret = response["SecretBinary"]
+    with open(env_file_path, "w") as env_file:
+        env_file.write(secret)
+    print("HERE IS THE SECRET", secret)
+    return "Success"
 
-# Model configuration
-# OPENAI_TEMPERATURE = float(os.getenv("OPENAI_TEMPERATURE", 0.0))
 
-#
-# DOTENV_EXTENSIONS = os.getenv("DOTENV_EXTENSIONS", "").split(" ")
-
-# Command line arguments extension
-# Can override any of the above environment variables
-# ENABLE_COMMAND_LINE_ARGS = (
-#     os.getenv("ENABLE_COMMAND_LINE_ARGS", "false").lower() == "true"
-# )
-# if ENABLE_COMMAND_LINE_ARGS:
-#     if can_import("extensions.argparseext"):
-#         from extensions.argparseext import parse_arguments
-#
-#         OBJECTIVE, INITIAL_TASK, OPENAI_API_MODEL, DOTENV_EXTENSIONS = parse_arguments()
-
-# Load additional environment variables for enabled extensions
-# if DOTENV_EXTENSIONS:
-#     if can_import("extensions.dotenvext"):
-#         from extensions.dotenvext import load_dotenv_extensions
-#
-#         load_dotenv_extensions(DOTENV_EXTENSIONS)
-print("Talk to the AI!")
-# if API_ENABLED:
+env_file = ".env"
+if os.path.exists(env_file):
+    # Load default environment variables (.env)
+    load_dotenv()
+    print("Talk to the AI!")
+    # if API_ENABLED:
     # Run FastAPI application
-start_api_server()
+    start_api_server()
 
-# else:
-#     pass
-# else    while True:
-#         userInput = input()
-#         if userInput:
-#             if (userInput.startswith("read:")):
-#                 agent.read(" ".join(userInput.split(" ")[1:]))
-#                 print("Understood! The information is stored in my memory.")
-#             elif (userInput.startswith("think:")):
-#                 agent.think(" ".join(userInput.split(" ")[1:]))
-#                 print("Understood! I stored that thought into my memory.")
-#             else:
-#                 print(agent.action(userInput), "\n")
-#         else:
-#             print("SYSTEM - Give a valid input")
+else:
+    secrets = fetch_secret("promethai-dev-backend-secretso-promethaijs-dotenv", "eu-west-1", ".env")
+    if secrets:
+        print(secrets)
+    load_dotenv()
+    start_api_server()
