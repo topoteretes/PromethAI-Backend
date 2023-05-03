cf6229c7723451c7e91d4c83228cc31508cae260
vasa <vas.markovic@gmail.com>
2023-05-03 20:22:54 +0200

    Remove print statement, we don't want to print secrets in prod

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,14 @@ COPY pyproject.toml poetry.lock /app/
 RUN poetry config virtualenvs.create false && \
     poetry install --no-root --no-dev
 
+RUN apt-get update -q && \
+    apt-get install curl zip jq netcat -y -q && \
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
+    unzip -qq awscliv2.zip && ./aws/install && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+
 RUN playwright install
 RUN playwright install-deps
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from api import start_api_server
 API_ENABLED = os.environ.get("API_ENABLED", "False").lower() == "true"
 import boto3
-
+import shutil
 
 def fetch_secret(secret_name, region_name, env_file_path):
     session = boto3.session.Session()
@@ -20,9 +20,23 @@ def fetch_secret(secret_name, region_name, env_file_path):
         secret = response["SecretString"]
     else:
         secret = response["SecretBinary"]
+
     with open(env_file_path, "w") as env_file:
         env_file.write(secret)
-    return "Success"
+
+    if os.path.exists(env_file_path):
+        print(f"The .env file is located at: {os.path.abspath(env_file_path)}")
+
+        # Move the .env file to the 'app' folder if it's not already there
+        app_folder = 'app'
+        target_path = os.path.join(app_folder, os.path.basename(env_file_path))
+
+        if os.path.abspath(env_file_path) != os.path.abspath(target_path):
+            shutil.move(env_file_path, target_path)
+            print(f"The .env file has been moved to: {os.path.abspath(target_path)}")
+    else:
+        print("The .env file was not found.")
+    return "Success in loading env files"
 
 
 env_file = ".env"
@@ -35,7 +49,7 @@ if os.path.exists(env_file):
     start_api_server()
 
 else:
-    secrets = fetch_secret("promethai-dev-backend-secretso-promethaijs-dotenv", "eu-west-1", ".env")
+    secrets = fetch_secret("promethai-dev-backend-secretso-promethaijs-dotenv", "eu-west-1", "/usr/src/app/.env")
     if secrets:
         print(secrets)
     load_dotenv()
