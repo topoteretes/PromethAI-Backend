d8611bc4342104e1e64d83ba02eebd601a629ca9
vasa <vas.markovic@gmail.com>
2023-05-03 15:20:16 +0200

    Removed assertion errors

--- main.py
+++ main.py
@@ -26,34 +26,34 @@ def can_import(module_name):
 # agent.createIndex()
 # API Keys
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
+# assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
 
 OPENAI_API_MODEL = os.getenv("OPENAI_API_MODEL", "gpt-3.5-turbo")
-assert OPENAI_API_MODEL, "OPENAI_API_MODEL environment variable is missing from .env"
+# assert OPENAI_API_MODEL, "OPENAI_API_MODEL environment variable is missing from .env"
 
 # Model configuration
 OPENAI_TEMPERATURE = float(os.getenv("OPENAI_TEMPERATURE", 0.0))
 
-
-DOTENV_EXTENSIONS = os.getenv("DOTENV_EXTENSIONS", "").split(" ")
+#
+# DOTENV_EXTENSIONS = os.getenv("DOTENV_EXTENSIONS", "").split(" ")
 
 # Command line arguments extension
 # Can override any of the above environment variables
-ENABLE_COMMAND_LINE_ARGS = (
-    os.getenv("ENABLE_COMMAND_LINE_ARGS", "false").lower() == "true"
-)
-if ENABLE_COMMAND_LINE_ARGS:
-    if can_import("extensions.argparseext"):
-        from extensions.argparseext import parse_arguments
-
-        OBJECTIVE, INITIAL_TASK, OPENAI_API_MODEL, DOTENV_EXTENSIONS = parse_arguments()
+# ENABLE_COMMAND_LINE_ARGS = (
+#     os.getenv("ENABLE_COMMAND_LINE_ARGS", "false").lower() == "true"
+# )
+# if ENABLE_COMMAND_LINE_ARGS:
+#     if can_import("extensions.argparseext"):
+#         from extensions.argparseext import parse_arguments
+#
+#         OBJECTIVE, INITIAL_TASK, OPENAI_API_MODEL, DOTENV_EXTENSIONS = parse_arguments()
 
 # Load additional environment variables for enabled extensions
-if DOTENV_EXTENSIONS:
-    if can_import("extensions.dotenvext"):
-        from extensions.dotenvext import load_dotenv_extensions
-
-        load_dotenv_extensions(DOTENV_EXTENSIONS)
+# if DOTENV_EXTENSIONS:
+#     if can_import("extensions.dotenvext"):
+#         from extensions.dotenvext import load_dotenv_extensions
+#
+#         load_dotenv_extensions(DOTENV_EXTENSIONS)
 print("Talk to the AI!")
 if API_ENABLED:
     # Run FastAPI application
