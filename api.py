640f64550bb8cd706ed740778f77fef172209a61
vasa <vas.markovic@gmail.com>
2023-06-09 15:51:12 +0200

    revert changes

--- api.py
+++ api.py
@@ -48,6 +48,23 @@ async def root():
 
 
 
+@app.post("/prompt-to-choose-meal-tree", response_model=dict)
+async def prompt_to_choose_meal_tree(request_data: Payload) -> dict:
+    if CANNED_RESPONSES:
+        with open('fixtures/choose_meal_tree_response.json', 'r') as f:
+            json_data = json.load(f)
+            stripped_string_dict = {"response": json_data}
+            return JSONResponse(content=stripped_string_dict)
+
+
+    json_payload = request_data.payload
+    agent = Agent()
+    agent.set_user_session(json_payload["user_id"], json_payload["session_id"])
+    output = agent.prompt_to_choose_meal_tree(json_payload["prompt"], model_speed= json_payload["model_speed"])
+
+    return JSONResponse(content={"response":output})
+from typing import Any, Generator
+from starlette.responses import StreamingResponse
 @app.post("/prompt-to-decompose-meal-tree-categories", response_model=dict)
 async def prompt_to_decompose_meal_tree_categories(request_data: Payload)-> dict:
 
