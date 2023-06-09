b8b77999c0ccabd1460bfdcd4bdb2059cef7c03a
Viktor Belényesi <bvic23@gmail.com>
2023-06-09 10:54:02 +0200

    Convert to JSON

--- api.py
+++ api.py
@@ -38,6 +38,28 @@ class ImageResponse(BaseModel):
 async def root():
     return {"message": "Hello, World, I am alive!"}
 
+def splitter(t):
+    list = t.split("=")
+    key = list[0].strip()
+    value = list[1].strip()
+    return { "category": key,
+             "options": [{
+                 "category": value,
+                 "options": []
+             }],
+            "preference": [value] 
+    }
+
+@app.post("/prompt-to-choose-meal-tree", response_model=dict)
+async def prompt_to_choose_meal_tree(request_data: Payload) -> dict:
+    json_payload = request_data.payload
+    agent = Agent()
+    agent.set_user_session(json_payload["user_id"], json_payload["session_id"])
+    output = agent.prompt_to_choose_meal_tree(json_payload["prompt"], model_speed= json_payload["model_speed"])
+
+    result = json.dumps({"results": list(map(splitter, output.replace('"', '').split(";")))})
+
+    return JSONResponse(content={"response":json.loads(result)})
 
 
 @app.post("/prompt-to-decompose-meal-tree-categories", response_model=dict)
@@ -100,22 +122,6 @@ async def voice_input(request_data: Payload) -> dict:
     agent = Agent()
     agent.set_user_session(json_payload["user_id"], json_payload["session_id"])
     output = agent.voice_input(query=json_payload["query"], model_speed= json_payload["model_speed"])
-    return JSONResponse(content={"response":json.loads(output)})
-
-@app.post("/prompt-to-choose-meal-tree", response_model=dict)
-async def prompt_to_choose_meal_tree(request_data: Payload) -> dict:
-    if CANNED_RESPONSES:
-        with open('fixtures/choose_meal_tree_response.json', 'r') as f:
-            json_data = json.load(f)
-            stripped_string_dict = {"response": json_data}
-            return JSONResponse(content=stripped_string_dict)
-
-
-    json_payload = request_data.payload
-    agent = Agent()
-    agent.set_user_session(json_payload["user_id"], json_payload["session_id"])
-    output = agent.prompt_to_choose_meal_tree(json_payload["prompt"], model_speed= json_payload["model_speed"])
-
     return JSONResponse(content={"response":json.loads(output)})
 
 @app.get("/health")
