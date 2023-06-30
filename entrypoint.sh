d112467c8748ba801171561b52c36a9877803dca
vasa <vas.markovic@gmail.com>
2023-06-30 09:30:14 +0200

    Added working version of Auth with example, refactor of the API to support adding additional assistants

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ export ENVIRONMENT
 python fetch_secret.py
 
 # Start Gunicorn
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker -t 120 --bind=0.0.0.0:8000 --log-level debug  api:app
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker -t 120 --bind=0.0.0.0:8000 --bind=0.0.0.0:443 --log-level debug  api:app
