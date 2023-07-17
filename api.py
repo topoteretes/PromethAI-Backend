05a120f4fffd8b1a2c379266575eba7771b8c8cb
Vasilije <8619304+Vasilije1990@users.noreply.github.com>
2023-07-17 13:15:35 +0200

    speed improvement for chains

--- api.py
+++ api.py
@@ -194,17 +194,17 @@ async def prompt_to_choose_meal_tree(request_data: Payload) -> dict:
     json_payload = request_data.payload
     agent = Agent()
     agent.set_user_session(json_payload["user_id"], json_payload["session_id"])
-    output = agent.prompt_to_choose_tree(
+    output = await agent.prompt_to_choose_tree(
         json_payload["prompt"],
         model_speed=json_payload["model_speed"],
         assistant_category="food",
     )
     logging.info("HERE IS THE CHAIN RESULT %s", output)
-    result = json.dumps(
-        {"results": list(map(splitter, output.replace('"', "").split(";")))}
-    )
-    return JSONResponse(content={"response": json.loads(result)})
-
+    # result = json.dumps(
+    #     {"results": list(map(splitter, output.replace('"', "").split(";")))}
+    # )
+    # return JSONResponse(content={"response": json.loads(result)})
+    return JSONResponse(content={output})
 def create_endpoint_with_resources(category: str, solution_type: str, prompt: str, json_example: str, *args, **kwargs):
     class Payload(BaseModel):
         payload: Dict[str, Any]
